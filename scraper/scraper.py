4ff609348a6dda07562278db6c566ae9a3c1c101
Dominik Bacher <dominikbacher@icloud.com>
2023-10-11 22:51:29 +0200

    Major code refactor

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,101 +1,141 @@
-import requests
-from bs4 import BeautifulSoup
-import random
-from time import sleep
-import json
 import os
+import sys
+import json
+import random
 import logging
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+import yaml
 
-MAX_IDS = 10000000
-SLEEP_MIN = 0.1
-SLEEP_MAX = 0.4
-BASE_URL = "https://www.imdb.com/title/tt{}"
-USER_AGENTS = [
-    # Google Chrome (Desktop)
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-    # Mozilla Firefox (Desktop)
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0",
-    # Apple Safari (Desktop)
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15",
-    # Microsoft Edge (Desktop)
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59",
-    # Opera (Desktop)
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 OPR/76.0.4017.123",
-]
+logging.basicConfig(
+    filename='app.log',
+    filemode='a',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
-logging.basicConfig(filename='app.log', filemode='w', level=logging.INFO)
-session = requests.Session()
+def load_config():
+    try:
+        with open('scraper/settings.yaml', 'r') as file:
+            config = yaml.safe_load(file)["settings"]
+        return config
+    except KeyError as ke:
+        logging.error(f"Missing key in configuration: {ke}")
+        sys.exit(1)
 
-def fetch_single_movie_data(url_id):
-    """Fetch data for a single movie given its URL ID."""
-    url = BASE_URL.format(url_id)
-    headers = {'User-Agent': random.choice(USER_AGENTS)}
+CONFIG = load_config()
+MAX_IDS = CONFIG["MAX_IDS"]
+USER_AGENTS = CONFIG["USER_AGENTS"]
+SLEEP_MIN = CONFIG["SLEEP_MIN"]
+SLEEP_MAX = CONFIG["SLEEP_MAX"]
+BASE_URL = CONFIG["BASE_URL"]
+
+def get_random_user_agent():
+    """Return a random user agent from a predefined list."""
+    return random.choice(USER_AGENTS)
+
+def fetch_url_content(url, session):
+    """Fetches content for a given URL."""
+    headers = {'User-Agent': get_random_user_agent()}
     try:
         response = session.get(url, headers=headers, timeout=15)
         response.raise_for_status()
-        if response.status_code == 200:
-            return response.text, None
-        else:
-            return None, "No Data Inside"
+        return response.text, None
 
     except requests.exceptions.HTTPError as he:
         if response.status_code != 404:
             logging.error(f"Error for {url_id}: {he}")
-
-        if response.status_code == 429:  # Rate limit, increase sleep
-            logging.error(f"Error for {url_id}: {he} | Sleep for 10 seconds")
+        if response.status_code == 429:
+            logging.error(f"Rate limited for {url_id}: {he} \nSleeping for 10 seconds.")
             sleep(10)
         return None, str(he)
 
     except requests.exceptions.RequestException as error:
-        logging.error(f"RequestException for {url}: {error}")
+        logging.error(f"RequestException for {url_id}: {error}")
         return None, str(error)
 
-def fetch_movie_data(start_idx=0, end_idx=MAX_IDS):
+def parse_movie_data(content):
+    """Parses movie data from the fetched content."""
+    soup = BeautifulSoup(content, "html.parser")
+    main_content = soup.find('main', attrs={"role": "main"})
+    return str(main_content) if main_content else None
+
+def fetch_movie_data(start_idx=0, end_idx=MAX_IDS, session=None):
     """Fetch data for a range of movies."""
     raw_content = {}
     missing_movies = {}
 
-    # Check if the files exist and load their content
-    if os.path.exists("raw_content.json"):
-        with open("raw_content.json", 'r', encoding='utf-8') as file:
-            raw_content = json.load(file)
-    if os.path.exists("missing_movies.json"):
-        with open("missing_movies.json", 'r', encoding='utf-8') as file:
-            missing_movies = json.load(file)
-    if raw_content or missing_movies:
-        highest_in_raw = max(map(int, raw_content.keys())) if raw_content else 0
-        highest_in_missing = max(map(int, missing_movies.keys())) if missing_movies else 0
-        start_idx = max(highest_in_raw, highest_in_missing)
-
     try:
+        print("Scraping initialized...")
         for i in range(start_idx, end_idx):
             url_id = str(i).zfill(len(str(MAX_IDS)) - 1)
-            content, error = fetch_single_movie_data(url_id)
+            url = BASE_URL.format(url_id)
+            content, error = fetch_url_content(url, session)
+
             if content:
-                raw_content[url_id] = content
+                movie_data = parse_movie_data(content)
+                if movie_data:
+                    raw_content[url_id] = movie_data
+                else:
+                    missing_movies[url_id] = "No Content Found"
             if error:
                 missing_movies[url_id] = error
-            if (i + 1) % 50 == 0:
+            if (i + 1) % 200 == 0:
                 logging.info(f"Processed {i + 1} movies")
                 print(f"Processed {i + 1} movies")
             sleep(random.uniform(SLEEP_MIN, SLEEP_MAX))
+
     except KeyboardInterrupt:
-        print("Fetching interrupted by user.")
-        print(f"Last movie ID scraped: {i}")
+        print("\nFetching interrupted by user.")
+        print(f"Last movie ID scraped: {i-1}")
     return raw_content, missing_movies
+
+def load_json_data(filename):
+    """Load data from JSON if it exists."""
+    try:
+        if os.path.exists(filename):
+            print(f"Loading existing data from {filename}...")
+            with open(filename, 'r', encoding='utf-8') as file:
+                return json.load(file)
+        return {}
+
+    except KeyboardInterrupt:
+        print("\nFetching interrupted by user before loading the data.")
+        sys.exit("Terminating the script to avoid data loss.")
 
 def save_to_json(filename, data):
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
 
+def find_latest_fetch(*args):
+    highest_id = 0
+    for data in args:
+        latest_id = max(map(int, data.keys())) if data else 0
+        if latest_id > highest_id:
+            highest_id = latest_id 
+    return highest_id
+
 def main():
-    print("Starting scraping process...")
-    raw_content, missing_movies = fetch_movie_data()
-    save_to_json("raw_content.json", raw_content)
-    save_to_json("missing_movies.json", missing_movies)
+    session = requests.Session()
+    raw_content = load_json_data("raw_content.json")
+    missing_movies = load_json_data("missing_movies.json")
+
+    try:
+        new_raw_content, new_missing_movies = fetch_movie_data(
+            start_idx=find_latest_fetch(raw_content, missing_movies),
+            session=session
+        )
+
+    except Exception as e:
+        logging.error(f"Error during data fetching: {e}")
+    else:
+        raw_content.update(new_raw_content)
+        missing_movies.update(new_missing_movies)
+
+    finally:
+        save_to_json("raw_content.json", raw_content)
+        save_to_json("missing_movies.json", missing_movies)
 
 if __name__ == "__main__":
     main()
