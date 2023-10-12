c3f421976ec16207acebebbf4525777e42aa97fe
Dominik Bacher <dominikbacher@icloud.com>
2023-10-12 11:34:02 +0200

    Select a child tag to store less data

--- scraper/data_parser.py
+++ scraper/data_parser.py
@@ -4,4 +4,5 @@ def parse_movie_data(content):
     """Parses movie data from the fetched content."""
     soup = BeautifulSoup(content, "html.parser")
     main_content = soup.find('main', attrs={"role": "main"})
-    return str(main_content) if main_content else None
+    section_content = main_content.find('div').find('section')
+    return str(section_content) if section_content else None
